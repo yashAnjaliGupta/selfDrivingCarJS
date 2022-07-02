60ad20f9ba8dd1c517eb31ccd3d7b76ccd77a1b6
yash <yashgupta.yg318@gmail.com>
2022-07-03 02:54:24 +0530

    neural network

--- car.js
+++ car.js
@@ -11,8 +11,14 @@ class Car{
         this.friction=0.05;
         this.angle=0;
         this.damaged=false;
+
+        this.useBrain=controlType=="AI";
+
         if(controlType!="DUMMY"){
             this.sensor= new Sensor(this);
+            this.brain=new NeuralNetwork(
+                [this.sensor.rayCount,6,4]
+            );
         }
         this.controls=new Controls(controlType);
         
@@ -26,6 +32,17 @@ class Car{
         }
         if(this.sensor){
             this.sensor.update(roadBorders,traffic);
+            const offsets=this.sensor.readings.map(
+                s=>s==null?0:1-s.offset
+            )
+            const outputs=NeuralNetwork.feedforward(offsets,this.brain);
+            console.log(outputs);
+            if(this.useBrain){
+                this.controls.forward=outputs[0];
+                this.controls.left=outputs[1];
+                this.controls.rigth=outputs[2];
+                this.controls.reverse=outputs[3];
+            }
         }
     }
     #assessDamage(roadBorders,traffic){
--- main.js
+++ main.js
@@ -1,22 +1,28 @@
 const canvas=document.getElementById("myCanvas");
 canvas.width=200;
 
+const networkCanvas=document.getElementById("networkCanvas");
+networkCanvas.width=300;
+
 const ctx=canvas.getContext("2d");
+const networkCtx=networkCanvas.getContext("2d");
+
 const road=new Road(canvas.width/2,canvas.width*0.9)
-const car=new Car(road.getLaneCenter(1),100,30,50,"KEYS");
+const car=new Car(road.getLaneCenter(1),100,30,50,"AI");
 // car.draw(ctx);
 const traffic=[
     new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",3),
 ];
 animate();
 
-function animate(){
+
+function animate(time){
     for(let i=0;i<traffic.length;i++){
         traffic[i].update(road.borders,[]);
     }
     car.update(road.borders,traffic);
     canvas.height=window.innerHeight;
-
+    networkCanvas.height=window.innerHeight;
     ctx.save();
     ctx.translate(0,-car.y+canvas.height*0.7);
 
@@ -27,5 +33,7 @@ function animate(){
     car.draw(ctx,"blue");
 
     ctx.restore();
+    networkCtx.lineDashOffset=-time/50;
+    Visualizer.drawNetwork(networkCtx,car.brain);
     requestAnimationFrame(animate);
 }
